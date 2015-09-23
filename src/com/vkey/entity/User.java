3b3113e6f85f304e7dd286c3484e876f33789c43
vkey <wuyonghui0810@126.com>
2015-09-23 16:52:38 +0800

    第二次提交新增两个Java类文件

---
+++ src/com/vkey/entity/User.java
@@ -0,0 +1,22 @@
+/**
+ * @author vkey
+ * @datetime 2015年9月23日 下午4:46:57
+ */
+package com.vkey.entity;
+public class User{
+	private String username;
+	private String password;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
---
+++ src/com/vkey/util/DBUtil.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.vkey.util;
+
+/**
+ * @author vkey
+ * @datetime 2015年9月23日 下午4:46:45
+ */
+public class DBUtil {
+
+}
