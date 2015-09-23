9aa3e8b1ac23f8420ddc0a2d1885421b47121f9b
vkey <wuyonghui0810@126.com>
2015-09-23 16:41:00 +0800

    首次提交到本地仓库

---
+++ src/com/vkey/util/TestGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.vkey.util;
+
+/**
+ * @author vkey
+ * @datetime 2015年9月23日 下午4:38:13
+ */
+public class TestGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
