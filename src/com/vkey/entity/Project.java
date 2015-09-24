c297ff8a5f797bcddbffe682f0a1199595c7c95a
vkeys <1134254946@qq.com>
2015-09-24 11:48:30 +0800

    vkey 提交

--- src/com/vkey/entity/Project.java
+++ src/com/vkey/entity/Project.java
@@ -8,14 +8,14 @@ package com.vkey.entity;
  * @datetime 2015年9月24日 上午8:37:59
  */
 public class Project {
-	private Integer id;
+	private Integer age;
 	private String username;
 	
-	public Integer getId() {
-		return id;
+	public Integer getAge() {
+		return age;
 	}
-	public void setId(Integer id) {
-		this.id = id;
+	public void setAge(Integer age) {
+		this.age = age;
 	}
 	public String getUsername() {
 		return username;
