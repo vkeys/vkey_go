4dfb37b61b24b80797ec20a95cb62d3429a96bc4
vkey <wuyonghui0810@126.com>
2015-09-24 08:42:30 +0800

    修改测试

--- src/com/vkey/entity/User.java
+++ src/com/vkey/entity/User.java
@@ -4,8 +4,16 @@
  */
 package com.vkey.entity;
 public class User{
+	private Integer id;
 	private String username;
 	private String password;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 	public String getUsername() {
 		return username;
 	}
