c23f81d51458f795996dfa8405a04867b9fb9fdc
vkey <wuyonghui0810@126.com>
2015-09-24 08:49:38 +0800

    commit vkey file

---
+++ src/com/vkey/entity/Project.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.vkey.entity;
+
+/**
+ * @author vkey
+ * @datetime 2015年9月24日 上午8:37:59
+ */
+public class Project {
+	private Integer id;
+	private String name;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
